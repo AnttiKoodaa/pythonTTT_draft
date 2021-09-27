4c728476191e785429a754c4fc8f532f1b02c02d
AnttiKoodaa <87748721+AnttiKoodaa@users.noreply.github.com>
2021-09-27 14:19:41 +0300

    Lisätty x -muuttuja

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ if __name__ == '__main__':
 	# go on forever
 	while True:
 		# Adding some comments
+		x = 3
 		print("Currently player::" + str(player))
 		row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
 		column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
