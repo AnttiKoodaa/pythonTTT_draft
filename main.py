c60d693661761874d940c9b12e889847b28f1379
AnttiKoodaa <87748721+AnttiKoodaa@users.noreply.github.com>
2021-09-27 13:24:58 +0300

    Adding extra :-letter

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 
 	# go on forever
 	while True:
-		print("Currently player: " + str(player))
+		print("Currently player::" + str(player))
 		row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
 		column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
 		game = add_piece(game, player, row, column)
